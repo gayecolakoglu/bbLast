c2e0a05aab20c2dd3c6ca3af770dad7e8b37ff27
Gaye Colakoglu <gayecolakogluuu@gmail.com>
2022-01-06 21:12:56 +0300

    last

---
+++ app/src/main/java/com/example/bb/RegisterUser.java
@@ -0,0 +1,76 @@
+package com.example.bb;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegisterUser extends AppCompatActivity implements View.OnClickListener{
+
+
+    private EditText editTextEmail,editTextPassword;
+    private Button sign;
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+
+        editTextEmail = (EditText) findViewById(R.id.username);
+        editTextPassword = (EditText) findViewById(R.id.password1);
+        sign = (Button) findViewById(R.id.sign);
+        sign.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.sign:
+                registerUser();
+                break;
+
+        }
+    }
+
+    private void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    User user = new User(email,password, mAuth.getUid());
+                    FirebaseDatabase.getInstance().getReference("Users")
+                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                            .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(RegisterUser.this, "User has been registered successfully..", Toast.LENGTH_SHORT).show();
+                            }else{
+                                Toast.makeText(RegisterUser.this, "Failed to registered..", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }else{
+                    Toast.makeText(RegisterUser.this, "Failed to registered..", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
